57c4221f43703b0699ea60bfcc1b4e370c177276
ashishsingh999 <asashish999@gmail.com>
2023-10-20 01:55:49 +0530

    feat: created demo landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-const x = 23;
-console.log("🚀 ~ file: App.jsx:2 ~ x:", x)
-
+import Demo from './components/demo';
+import Hero from './components/Hero';
+import './App.css';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;
