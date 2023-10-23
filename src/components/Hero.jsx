9b94b009f5f11052491fbae69dd4c41e69eb1871
ashishsingh999 <asashish999@gmail.com>
2023-10-23 22:32:11 +0530

    feat: implemented entire app
    setup redux store, api setup using rtk query
    
    used local storage for data persistence

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
-
+import { logo } from '../assets';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() =>
+            window.open('https://github.com/ashishsingh999/ai-summarizer')
+          }
+          className="rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
